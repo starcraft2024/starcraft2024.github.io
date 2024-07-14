ad87e19f9f3097b07eb0e5fc992d6580d4760928
starcraft2024 <33438743@qq.com>
2024-07-14 21:34:04 +0800

    Site updated: 2024-07-14 21:34:04

--- js/snowfall.js
+++ js/snowfall.js
@@ -1,75 +1,82 @@
-var stop, staticx;
-var img = new Image();
-img.src = "leaf.png"; // 本地树叶图片的路径
+// falling-leaves.js
 
-function Leaf(x, y, s, r, fn) {
-    this.x = x;
-    this.y = y;
-    this.s = s;
-    this.r = r;
-    this.fn = fn;
+// 初始化 Canvas 和上下文
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas_leaves';
+canvas.style.position = 'fixed';
+canvas.style.left = '0';
+canvas.style.top = '0';
+canvas.style.pointerEvents = 'none';
+document.body.appendChild(canvas);
+const cxt = canvas.getContext('2d');
+
+const leaves = []; // 用于存储树叶对象
+
+const leafImage = new Image();
+leafImage.src = 'leaf.png'; // 修改为你本地的树叶图片路径
+
+class Leaf {
+    constructor(x, y, s, r, fn) {
+        this.x = x; // 树叶的 x 坐标
+        this.y = y; // 树叶的 y 坐标
+        this.s = s; // 树叶的大小
+        this.r = r; // 树叶的旋转角度
+        this.fn = fn; // 树叶的运动轨迹函数
+    }
+
+    draw() {
+        cxt.save();
+        const size = 50 * this.s; // 树叶大小
+        cxt.translate(this.x, this.y);
+        cxt.rotate(this.r);
+        cxt.drawImage(leafImage, -size / 2, -size / 2, size, size);
+        cxt.restore();
+    }
+
+    update() {
+        this.x = this.fn.x(this.x, this.y);
+        this.y = this.fn.y(this.y, this.y);
+        this.r = this.fn.r(this.r);
+        if (this.x > window.innerWidth || this.x < 0 || this.y > window.innerHeight || this.y < 0) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.x = getRandom('x');
+        this.y = 0;
+        this.s = getRandom('s');
+        this.r = getRandom('r');
+    }
 }
 
-Leaf.prototype.draw = function(cxt) {
-    cxt.save();
-    var xc = 40 * this.s / 4;
-    cxt.translate(this.x, this.y);
-    cxt.rotate(this.r);
-    cxt.drawImage(img, 0, 0, 40 * this.s, 40 * this.s);
-    cxt.restore();
-}
+class LeafList {
+    constructor() {
+        this.list = [];
+    }
 
-Leaf.prototype.update = function() {
-    this.x = this.fn.x(this.x, this.y);
-    this.y = this.fn.y(this.y, this.y);
-    this.r = this.fn.r(this.r);
-    if(this.x > window.innerWidth || this.x < 0 || this.y > window.innerHeight || this.y < 0) {
-        this.r = getRandom('fnr');
-        if(Math.random() > 0.4) {
-            this.x = getRandom('x');
-            this.y = 0;
-            this.s = getRandom('s');
-            this.r = getRandom('r');
-        } else {
-            this.x = window.innerWidth;
-            this.y = getRandom('y');
-            this.s = getRandom('s');
-            this.r = getRandom('r');
+    push(leaf) {
+        this.list.push(leaf);
+    }
+
+    update() {
+        for (const leaf of this.list) {
+            leaf.update();
+        }
+    }
+
+    draw() {
+        for (const leaf of this.list) {
+            leaf.draw();
         }
     }
 }
 
-function LeafList() {
-    this.list = [];
-}
-
-LeafList.prototype.push = function(leaf) {
-    this.list.push(leaf);
-}
-
-LeafList.prototype.update = function() {
-    for(var i = 0, len = this.list.length; i < len; i++) {
-        this.list[i].update();
-    }
-}
-
-LeafList.prototype.draw = function(cxt) {
-    for(var i = 0, len = this.list.length; i < len; i++) {
-        this.list[i].draw(cxt);
-    }
-}
-
-LeafList.prototype.get = function(i) {
-    return this.list[i];
-}
-
-LeafList.prototype.size = function() {
-    return this.list.length;
-}
-
+// 生成随机数或函数
 function getRandom(option) {
-    var ret, random;
-    switch(option) {
+    let ret;
+    const random = Math.random();
+    switch (option) {
         case 'x':
             ret = Math.random() * window.innerWidth;
             break;
@@ -77,82 +84,60 @@ function getRandom(option) {
             ret = Math.random() * window.innerHeight;
             break;
         case 's':
-            ret = Math.random();
+            ret = random;
             break;
         case 'r':
-            ret = Math.random() * 6;
+            ret = random * 2 * Math.PI;
             break;
         case 'fnx':
-            random = -0.5 + Math.random() * 1;
-            ret = function(x, y) {
-                return x + 0.5 * random - 1.7;
-            };
+            ret = () => -0.5 + random * 1;
             break;
         case 'fny':
-            random = 1.5 + Math.random() * 0.7;
-            ret = function(x, y) {
-                return y + random;
-            };
+            ret = () => 0.5 + random * 2;
             break;
         case 'fnr':
-            random = Math.random() * 0.03;
-            ret = function(r) {
-                return r + random;
-            };
+            ret = () => random * 0.02 - 0.01;
             break;
     }
-    return ret;
+    return typeof ret === 'function' ? ret : () => ret;
 }
 
-function startLeaves() {
-    requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame;
-    var canvas = document.createElement('canvas'), cxt;
-    staticx = true;
-    canvas.height = window.innerHeight;
+// 启动飘落效果
+function startFallingLeaves() {
     canvas.width = window.innerWidth;
-    canvas.setAttribute('style', 'position: fixed; left: 0; top: 0; pointer-events: none;');
-    canvas.setAttribute('id', 'canvas_leaves');
-    document.getElementsByTagName('body')[0].appendChild(canvas);
-    cxt = canvas.getContext('2d');
-    var leafList = new LeafList();
-    for(var i = 0; i < 50; i++) {
-        var leaf, randomX, randomY, randomS, randomR, randomFnx, randomFny;
-        randomX = getRandom('x');
-        randomY = getRandom('y');
-        randomR = getRandom('r');
-        randomS = getRandom('s');
-        randomFnx = getRandom('fnx');
-        randomFny = getRandom('fny');
-        randomFnR = getRandom('fnr');
-        leaf = new Leaf(randomX, randomY, randomS, randomR, {x: randomFnx, y: randomFny, r: randomFnR});
-        leaf.draw(cxt);
+    canvas.height = window.innerHeight;
+
+    const leafList = new LeafList();
+    for (let i = 0; i < 50; i++) {
+        const leaf = new Leaf(
+            getRandom('x'),
+            getRandom('y'),
+            getRandom('s'),
+            getRandom('r'),
+            {
+                x: getRandom('fnx'),
+                y: getRandom('fny'),
+                r: getRandom('fnr')
+            }
+        );
         leafList.push(leaf);
     }
-    stop = requestAnimationFrame(function() {
+
+    function animate() {
         cxt.clearRect(0, 0, canvas.width, canvas.height);
         leafList.update();
-        leafList.draw(cxt);
-        stop = requestAnimationFrame(arguments.callee);
-    });
-}
-
-window.onresize = function() {
-    var canvasLeaves = document.getElementById('canvas_leaves');
-    canvasLeaves.width = window.innerWidth;
-    canvasLeaves.height = window.innerHeight;
-}
-
-img.onload = function() {
-    startLeaves();
-}
-
-function stopLeaves() {
-    if(staticx) {
-        var child = document.getElementById("canvas_leaves");
-        child.parentNode.removeChild(child);
-        window.cancelAnimationFrame(stop);
-        staticx = false;
-    } else {
-        startLeaves();
+        leafList.draw();
+        requestAnimationFrame(animate);
     }
+
+    animate();
 }
+
+// 处理窗口大小变化
+window.onresize = function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+// 加载树叶图片并启动效果
+leafImage.onload = startFallingLeaves;
